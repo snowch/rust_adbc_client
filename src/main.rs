ce08da02c1de353c42d64662fdd56b6a42eb19e7
snowch <chsnow123@gmail.com>
2025-02-21 16:02:20 +0000

    initial import

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use adbc_core::{Driver, Statement}; 
+use adbc_core::{Database, Connection};
+use adbc_dummy::DummyDriver;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create the driver
+    let mut driver = DummyDriver::default();  // Changed from new() to default()
+    
+    // Create database connection
+    let mut db = driver.new_database()?;
+    
+    // Create connection
+    let mut conn = db.new_connection()?;
+    
+    // Create and execute a statement
+    let mut stmt = conn.new_statement()?;
+    stmt.set_sql_query("SELECT 1")?;
+    let results = stmt.execute()?;
+    
+    // Print results
+    for batch in results {
+        println!("Batch: {:?}", batch?);
+    }
+    
+    Ok(())
+}
